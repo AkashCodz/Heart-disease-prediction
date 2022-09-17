e7b09b22793c8a4762ee1f30f202e08c8d198b41
Akash Sharma <101062599+AkashCodz@users.noreply.github.com>
2022-09-17 19:21:19 +0530

    Add files via upload

---
+++ hdt/enroll/views.py
@@ -0,0 +1,87 @@
+from django.shortcuts import render, redirect  
+from .forms import userforms       
+from .forms import signupform     # Signup    
+from django.contrib import messages     # Messages    
+from django.contrib.auth.forms import AuthenticationForm  #Login
+from django.contrib.auth import authenticate, login, logout
+
+
+# Signup
+def sign_up(request):
+    if request.method == 'POST':
+        fm=signupform(request.POST)
+        if fm.is_valid():
+            messages.success(request,'Account Created Successfully !!')
+            fm.save()
+    else:
+        fm=signupform()
+    return render(request, 'enroll\signup.html',{'form':fm})
+
+# Login
+def user_login(request):
+    if not request.user.is_authenticated:
+        if request.method=="POST":
+            fm=AuthenticationForm(request=request, data=request.POST)
+            if fm.is_valid():
+                uname=fm.cleaned_data['username']
+                upass=fm.cleaned_data['password']
+                user=authenticate(username=uname, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request,'Logged out successfully !!')
+                    return redirect('/home/')
+        else:
+            fm=AuthenticationForm()
+        return render(request, "enroll/userlogin.html", {'form':fm})
+    else:
+        return redirect('/home/')
+
+# Profile
+
+# def user_profile(request):
+#     if request.user.is_authenticated:
+#         return render(request, 'enroll/profile.html', {'name':request.user})
+#     else:
+#         return redirect('/login/')
+    
+# Logout
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+# Main file
+def home(request):
+    if request.user.is_authenticated:
+        fn=userforms()
+        data={'form':fn}
+        return render(request,'enroll\index.html',data)
+    else:
+        return redirect('/login/')
+        # return render(request, 'enroll/profile.html', {'name':request.user})
+
+# custom method for generating predictions
+def getPredictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal):
+    import pickle
+    model = pickle.load(open("enroll/static/enroll/Heart_disease_prediction.sav", "rb"))
+    prediction = model.predict([[age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal]])
+    return prediction
+        
+def result(request):
+    age = int(request.POST['age'])
+    sex = int(request.POST['sex'])
+    cp = int(request.POST['cp'])
+    trestbps = int(request.POST['trestbps'])
+    chol = int(request.POST['chol'])
+    fbs = int(request.POST['fbs'])
+    restecg = int(request.POST['restecg'])
+    thalach = int(request.POST['thalach'])
+    exang = int(request.POST['exang'])
+    oldpeak = float(request.POST['oldpeak'])
+    slope = int(request.POST['slope'])
+    ca = int(request.POST['ca'])
+    thal = int(request.POST['thal'])
+
+    result = getPredictions(age, sex, cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal)
+
+    return render(request, 'enroll/result.html', {'result':result})
+
